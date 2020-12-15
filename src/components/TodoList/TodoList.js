567f4b3fa140c6712e3da8fea2a9355572e95eda
Pavit <pavitpim@gmail.com>
2020-12-16 01:56:40 +0700

    Setting up Project

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import { List, Button, Input, Row, Col, Divider } from 'antd'
+import Text from 'antd/lib/typography/Text'
+import _ from 'lodash'
+import axios from '../../config/axios'
+import Todo from './Todo'
+
+export default function TodoList() {
+    const [todoList, setTodoList] = useState([])
+    const [inputField, setInputField] = useState("")
+    // GET
+    const fetchTodoList = async() => {
+        const httpResponse = await axios.get("/todo-list");
+        setTodoList(httpResponse.data)
+    };
+    useEffect(()=> {
+        fetchTodoList();
+    },[]);
+    
+    const addTodoItem = async() => {
+        await axios.post("/todo-list" ,{task: inputField})
+        fetchTodoList();
+
+        // const newTodoList = [...todoList]
+        // newTodoList.push({
+        //     id: _.uniqueId(),
+        //     task: inputField,
+        // })
+        // setTodoList(newTodoList)
+        // setInputField("")
+    }
+    const deleteTodoItem = async (id) => {
+        await axios.delete(`/todo-list/${id}`)
+        fetchTodoList();
+        // วิธี 1 Best Practice
+        // const newTodoList = todoList.filter(todo => todo.id !== id)
+        // setTodoList(newTodoList)
+
+        // const newTodoList = [...todoList]
+        // const targetIndex = newTodoList.findIndex(todo => todo.id === id)
+        // newTodoList.splice(targetIndex,1)
+        // setTodoList(newTodoList)
+    }
+    return (
+        <Row justify="center">
+            <Col>
+                <Row>
+                    <Text type="warning"> กรุณาใส่ Todo ที่ต้องการเพิ่ม</Text>
+                </Row>
+                <Row justify="center">
+                    <Col span={20}>
+                        <Input value={inputField} onChange={(e) => setInputField(e.target.value)} />
+                    </Col>
+                    <Col span={4}>
+                        <Button style={{ width: '100%' }} onClick={addTodoItem}>Add</Button>
+                    </Col>
+                </Row>
+                <Divider />
+                <Row>
+                    <List
+                        style={{ width: "450px" }}
+                        header={<div>Todo List Page</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={todoList}
+                        renderItem={todo => (
+                            <List.Item>
+                                <Todo delete={deleteTodoItem} todo={todo} fetchData={fetchTodoList}/>
+                            </List.Item>
+                        )}
+                    />
+                </Row>
+            </Col>
+        </Row>
+    )
+}
